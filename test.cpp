fd7177dc8990e6ab2fc4dde850230ba562e5eff7
Nguyễn Hà Thanh <139417886+thanhcode118@users.noreply.github.com>
2023-09-28 10:55:43 +0700

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+int main(){
+char kitu;
+cin>>kitu;
+if (kitu>=65 && kitu<=90) cout<<"YES";
+else  cout<<"NO";
+// cout<<fixed<<setprecision(2)<<c<<endl;
+}
